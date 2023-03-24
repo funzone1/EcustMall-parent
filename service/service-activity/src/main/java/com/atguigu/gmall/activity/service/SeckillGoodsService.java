3cde07380c77e51cef1c3f5b49fdd02203b729fc
funz <yuzaifzh98@163.com>
2023-03-24 13:49:40 +0800

    version-最终

---
+++ service/service-activity/src/main/java/com/atguigu/gmall/activity/service/SeckillGoodsService.java
@@ -0,0 +1,28 @@
+package com.atguigu.gmall.activity.service;
+
+import com.atguigu.gmall.model.activity.SeckillGoods;
+
+import java.util.List;
+
+public interface SeckillGoodsService {
+    /**
+     * 查询秒杀列表
+     * @return
+     */
+    List<SeckillGoods> findAll();
+
+    /**
+     * 商品详情
+     * @param skuId
+     * @return
+     */
+    SeckillGoods getSeckillGoods(Long skuId);
+
+    /**
+     * 抢单
+     * @param userId
+     * @param skuId
+     */
+    void seckillOrder(String userId, Long skuId);
+
+}
